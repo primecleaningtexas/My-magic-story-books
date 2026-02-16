5550454794f4a28d60472b2e0894914b9de69399
Alisson <primecleaningtexas@gmail.com>
2026-02-15 22:10:50 -0600

    Add slug and image properties to themes

--- components/themes.js
+++ components/themes.js
@@ -1,10 +1,10 @@
 export const THEMES = [
-  { id: "magical", title: "Magical Adventure" },
-  { id: "dreamland", title: "Dreamland Journey" },
-  { id: "undersea", title: "Under the Sea" },
-  { id: "space", title: "Space Explorer" },
-  { id: "animals", title: "Kingdom of Animals" },
-  { id: "numbers", title: "Numbers Quest" },
-  { id: "bedtime", title: "Bedtime Adventure" },
-  { id: "safari", title: "Amazing Safari" }
+  { id: "magical", title: "Magical Adventure", slug: "magical-adventure", image: "/themes/magical-adventure.jpg" },
+  { id: "dreamland", title: "Dreamland Journey", slug: "dreamland-journey", image: "/themes/dreamland-journey.jpg" },
+  { id: "sea", title: "Under the Sea", slug: "under-the-sea", image: "/themes/under-the-sea.jpg" },
+  { id: "space", title: "Space Explorer", slug: "space-explorer", image: "/themes/space-explorer.jpg" },
+  { id: "animals", title: "Kingdom of Animals", slug: "kingdom-of-animals", image: "/themes/kingdom-of-animals.jpg" },
+  { id: "numbers", title: "Numbers Quest", slug: "numbers-quest", image: "/themes/numbers-quest.jpg" },
+  { id: "bedtime", title: "Bedtime Adventure", slug: "bedtime-adventure", image: "/themes/bedtime-adventure.jpg" },
+  { id: "safari", title: "Amazing Safari", slug: "amazing-safari", image: "/themes/amazing-safari.jpg" },
 ];
